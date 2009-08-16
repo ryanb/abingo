a328f34e504f205930851bd843aeed129ba79630
Patrick McKenzie <patrick@bingocardcreator.com>
2009-08-16 17:03:08 +0900

    Fixing view helper so that it works without blocks.  Handy for <% if(ab_test("show_some_element") %> ... <% end %>

--- lib/abingo_view_helper.rb
+++ lib/abingo_view_helper.rb
@@ -22,8 +22,13 @@ module AbingoViewHelper
     else
       choice = Abingo.test(test_name, alternatives, options)
     end
-    content_tag = capture(choice, &block)
-    block_called_from_erb?(block) ? concat(content_tag) : content_tag
+
+    if block
+      content_tag = capture(choice, &block)
+      block_called_from_erb?(block) ? concat(content_tag) : content_tag
+    else
+      choice
+    end
   end
 
   def bingo!(test_name, options = {})
